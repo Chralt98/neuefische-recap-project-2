01a526de9027310fba1e4cf69c7472bb15d92db1
Christopher Altmann <contact@christopher-altmann.com>
2026-05-08 16:53:55 +0200

    feat: add prettier for code formatting and implement region and trail views with controllers

--- src/controllers/regionController.ts
+++ src/controllers/regionController.ts
@@ -1,10 +1,31 @@
 import { type Request, type Response } from "express";
+import { getAllRegions, getRegionBySlug } from "../models/regionModel";
+import { getTrailsByRegionId } from "../models/trailModel";
 
-export function showAllRegions(req: Request, res: Response) {
-  res.render("index.html");
+export async function showAllRegions(req: Request, res: Response) {
+  try {
+    const regions = await getAllRegions();
+    res.render("regions.html", { regions });
+  } catch (error) {
+    console.error(error);
+    res.send("Failed to fetch all regions");
+  }
 }
 
-export function showRegionWithTrailsBySlug(
+// shows a single region with its trails
+export async function showRegionWithTrailsBySlug(
   req: Request<{ slug: string }>,
   res: Response,
-) {}
+) {
+  try {
+    const region = await getRegionBySlug(req.params.slug);
+    if (!region) {
+      return res.status(404).send("Region not found");
+    }
+    const trails = await getTrailsByRegionId(region.id);
+    res.render("region.html", { trails });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Failed to fetch the region with trails");
+  }
+}
--- src/controllers/trailController.ts
+++ src/controllers/trailController.ts
@@ -1,12 +1,28 @@
 import { type Request, type Response } from "express";
+import { getAllTrails, getTrailBySlug } from "../models/trailModel";
 
-export function showAllTrails(req: Request, res: Response) {
-  res.render("index.html");
+export async function showAllTrails(req: Request, res: Response) {
+  try {
+    const trails = await getAllTrails();
+    res.render("index.html", { trails });
+  } catch (error) {
+    console.error(error);
+    res.send("Failed to fetch all trails");
+  }
 }
 
-export function showRegonizedTrailBySlug(
+export async function showRegonizedTrailBySlug(
   req: Request<{ slug: string }>,
   res: Response,
 ) {
-  res.render("index.html");
+  try {
+    const trail = await getTrailBySlug(req.params.slug);
+    if (!trail) {
+      return res.status(404).send("Trail not found");
+    }
+    res.render("trail.html", { trail });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Failed to fetch the trail");
+  }
 }
